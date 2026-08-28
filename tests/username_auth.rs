09f98d6f4b2c142223f8962163ea3b95a8916e48
just-Humanoid <322021698+just-Humanoid@users.noreply.github.com>
2026-08-28 11:30:27 +0400

    feat(test): add username-password tests

---
+++ tests/username_auth.rs
@@ -0,0 +1,65 @@
+mod common;
+
+use crate::common::{runtime, test_bind, test_connect, ECHO_SERVER_ADDR, PROXY_ADDR};
+#[cfg(unix)]
+use common::connect_unix;
+use better_socks::{
+    tcp::{Socks5Listener, Socks5Stream},
+    Result,
+};
+
+#[test]
+fn connect_username_auth() -> Result<()> {
+    let runtime = runtime().lock().unwrap();
+    let conn = runtime.block_on(Socks5Stream::connect_with_password(
+        PROXY_ADDR,
+        ECHO_SERVER_ADDR,
+        "mylogin",
+        "mypassword",
+    ))?;
+    runtime.block_on(test_connect(conn))
+}
+
+#[test]
+fn bind_username_auth() -> Result<()> {
+    let bind = {
+        let runtime = runtime().lock().unwrap();
+        runtime.block_on(Socks5Listener::bind_with_password(
+            PROXY_ADDR,
+            ECHO_SERVER_ADDR,
+            "mylogin",
+            "mypassword",
+        ))
+    }?;
+    test_bind(bind)
+}
+
+#[cfg(unix)]
+#[test]
+fn connect_with_socket_username_auth() -> Result<()> {
+    let runtime = runtime().lock().unwrap();
+    let socket = runtime.block_on(connect_unix())?;
+    let conn = runtime.block_on(Socks5Stream::connect_with_password_and_socket(
+        socket,
+        ECHO_SERVER_ADDR,
+        "mylogin",
+        "mypassword",
+    ))?;
+    runtime.block_on(test_connect(conn))
+}
+
+#[cfg(unix)]
+#[test]
+fn bind_with_socket_username_auth() -> Result<()> {
+    let bind = {
+        let runtime = runtime().lock().unwrap();
+        let socket = runtime.block_on(connect_unix())?;
+        runtime.block_on(Socks5Listener::bind_with_password_and_socket(
+            socket,
+            ECHO_SERVER_ADDR,
+            "mylogin",
+            "mypassword",
+        ))
+    }?;
+    test_bind(bind)
+}
